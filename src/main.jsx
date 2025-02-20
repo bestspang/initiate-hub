967dd108e2bbcea06732b650a915c2ea0130a283
Kongphop S <bestspang@gmail.com>
2025-02-20 22:44:21 +0700

    Build changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+//import App from "./App.jsx";
+import HomePage from "./pages/HomePage";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <HomePage />
+  </StrictMode>,
+);
+
+/*
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -7,3 +20,6 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>,
 );
+
+
+*/
