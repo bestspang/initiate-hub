f0b10154a0c8973522b35ed9550cc9d9a5244c70
Kongphop S <bestspang@gmail.com>
2025-02-21 01:14:42 +0700

    Build changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,20 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { componentTagger } from "lovable-tagger";
 import path from "path";
-import type { UserConfig } from 'vite';
+import type { UserConfig } from "vite";
 
 const config: UserConfig = {
   plugins: [
     react(),
     tailwindcss(),
-    process.env.NODE_ENV === 'development' && componentTagger(),
+    process.env.NODE_ENV === "development" && componentTagger(),
   ].filter(Boolean),
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "2b1e5db9-1d79-4369-82f0-8adb51b07b0d.lovableproject.com",
+    ],
   },
   resolve: {
     alias: {
@@ -24,3 +27,4 @@ const config: UserConfig = {
 };
 
 export default defineConfig(config);
+
